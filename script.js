cfa474bc768b4e4f2ea5d22b0a165504e8dbd9f1
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-20 23:03:46 -0300

    js aplicado para mudar páginas de criação

--- script.js
+++ script.js
@@ -3,5 +3,42 @@ function acessarQuizz(){
 }
 
 function criarQuizz(){
-//Esconder tela 1 e acessar tela 3
+    const esconderTela = document.querySelector(".conteudo-tela1");
+    const mostrarTela = document.querySelector(".conteudo-tela3");
+    esconderTela.classList.add("escondido");
+    mostrarTela.classList.remove("escondido");
+
+}
+
+function criarPerguntas(){
+    const esconderTela = document.querySelector(".comeca-pelo-comeco");
+    const mostrarTela = document.querySelector(".criar-perguntas");
+    esconderTela.classList.add("escondido");
+    mostrarTela.classList.remove("escondido");
+}
+
+function criarNiveis(){
+    const esconderTela = document.querySelector(".criar-perguntas");
+    const mostrarTela = document.querySelector(".escolha-niveis");
+    esconderTela.classList.add("escondido");
+    mostrarTela.classList.remove("escondido");
+}
+
+function finalizarQuizz(){
+    const esconderTela = document.querySelector(".escolha-niveis");
+    const mostrarTela = document.querySelector(".quizz-pronto");
+    esconderTela.classList.add("escondido");
+    mostrarTela.classList.remove("escondido");
+}
+
+function paginaPrincipal(){
+    const mostrarTelaPrincipal = document.querySelector(".conteudo-tela1");
+    const esconderTelaAtual = document.querySelector(".conteudo-tela3");
+    const alterarCriacao = document.querySelector(".quizz-pronto");
+    const mostrarPassoCriacao = document.querySelector(".comeca-pelo-comeco");
+    
+    mostrarTelaPrincipal.classList.remove("escondido");
+    esconderTelaAtual.classList.add("escondido");
+    alterarCriacao.classList.add("escondido");
+    mostrarPassoCriacao.classList.remove("escondido");
 }
