efc96b8b48c40f7f444fee2788c14f3f00e8370e
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-04-20 23:42:02 -0300

    JavaScript do Primeiro da primeira parte da Tela 2

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
+const tela1 = document.querySelector(".conteudo-tela1")
+const tela2 = document.querySelector(".conteudo-tela2")
+const tela3 = document.querySelector(".conteudo-tela3")
+let respostaEscolhida;
+let respostaCerta;
+let respostasErradas;
+let tentativas = 0;
+let acertos = 0;
+
+let teste;
+
 function acessarQuizz(){
-//Esconder tela 1 e acessar tela 2
+    tela1.classList.toggle("escondido")
+    tela2.classList.toggle("escondido")
 }
 
 function criarQuizz(){
-//Esconder tela 1 e acessar tela 3
+
 }
+
+function escolherResposta(ele){
+    teste = ele.nextElementSibling
+    if (ele.classList.contains("esbranquicado") || ele === respostaEscolhida){
+        return
+    }
+
+    const todasAlternativas = Array.from(ele.parentNode.children) 
+    todasAlternativas.forEach(alt => {
+        alt.classList.add("esbranquicado")
+    });
+    ele.classList.remove("esbranquicado")
+    respostaEscolhida = ele
+    correcao(todasAlternativas)
+    setTimeout(scrollar, 2000)
+    
+}
+
+function correcao(todasAlternativas){
+    respostaCerta = []
+    respostasErradas = []
+    todasAlternativas.forEach(comparaResposta)
+    if (respostaEscolhida === respostaCerta[0]){ acertos += 1}
+    tentativas += 1;
+    respostaCerta[0].classList.add("correto")
+    respostasErradas.forEach(alt => {
+        alt.classList.add("errado")
+    });
+    
+}
+
+function comparaResposta(alternativa){
+    if(alternativa === alternativa){
+        respostaCerta.push(alternativa)
+    } else {
+        respostasErradas.push(alternativa)
+    }  
+}
+
+function scrollar(){
+    const pergunta = document.querySelectorAll(".pergunta-quizz")
+    if(tentativas < pergunta.length){
+        pergunta[tentativas].scrollIntoView({block: "center", behavior: "smooth"})
+    }
+}
+
